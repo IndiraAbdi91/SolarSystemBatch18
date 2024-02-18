d2a0562eaf9fd57f4e93ebdcca30078143949fe1
IndiraAbdi91! <indira912017@gmail.com>
2024-02-18 07:05:03 -0500

    i have worked on planet1

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -11,6 +11,13 @@ public class SolarSystem {
         xros.Color="pink";
         xros.temp="2113545458";
         xros.Size="5146541561355";
+
+
+
+        Planet1 frog=new Planet1();
+        frog.color="blue";
+        frog.temp="12345678";
+        frog.size="2km";
         
 
     }
